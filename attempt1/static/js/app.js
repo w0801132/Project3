62e31f3c46029fbebb3e56a702e2f01e36dbd0d1
Melissa Wood <38088765+w0801132@users.noreply.github.com>
2019-01-10 18:04:31 -0500

    Add files via upload

--- attempt1/static/js/app.js
+++ attempt1/static/js/app.js
@@ -1,5 +1,5 @@
 // Plot the default route once the page loads
-var defaultURL = "/emoji_char";
+var defaultURL = "/botanical";
 d3.json(defaultURL).then(function(data) {
   var data = [data];
   var layout = { margin: { t: 30, b: 100 } };
