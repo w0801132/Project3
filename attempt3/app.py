671ae23ce906e4b8292b20f4734f313de9882150
Melissa Wood <38088765+w0801132@users.noreply.github.com>
2019-01-12 09:17:54 -0500

    Final
    We did a thing and it works

---
+++ attempt3/app.py
@@ -0,0 +1,218 @@
+import pandas as pd
+
+from flask import (
+    Flask,
+    render_template,
+    jsonify)
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
+
+app = Flask(__name__)
+
+# The database URI
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db/museums.sqlite"
+
+db = SQLAlchemy(app)
+
+#Create our database model
+class Museums(db.Model):
+    __tablename__ = 'funstuff'
+
+    museumid= db.Column(db.Integer, primary_key=True)
+    museumtype = db.Column(db.String)
+    state = db.Column(db.String)
+
+    def __repr__(self):
+        return '<Museums %r>' % (self.name)
+
+# Create database tables
+# @app.before_first_request
+# def setup():
+    # Recreate database each time for demo
+    # db.drop_all()
+    # db.create_all()
+
+
+@app.route("/")
+def home():
+    """Render Home Page."""
+    return render_template("index.html")
+
+
+@app.route("/botanical")
+def botanical_data():
+    """Return state and museum type"""
+
+    # Query for the count of museums by type, by state
+    results = db.session.query(Museums.state, func.count(Museums.museumtype)).\
+    filter_by(museumtype = 'ARBORETUM, BOTANICAL GARDEN, OR NATURE CENTER').\
+    group_by(Museums.state).all()
+
+    df = pd.DataFrame(results, columns=['states', 'type_count'])
+
+    # Format the data for Plotly
+    trace = {
+        "x": df["states"].values.tolist(),
+        "y": df["type_count"].values.tolist(),
+        "type": "bar"
+    }
+    return jsonify(trace)
+
+
+@app.route("/art")
+def art_data():
+    """Return state and museum type"""
+
+    # Query for the count of museums by type, by state
+    results = db.session.query(Museums.state, func.count(Museums.museumtype)).\
+    filter_by(museumtype = 'ART MUSEUM').\
+    group_by(Museums.state).all()
+
+    df = pd.DataFrame(results, columns=['states', 'type_count'])
+
+    # Format the data for Plotly
+    trace = {
+        "x": df["states"].values.tolist(),
+        "y": df["type_count"].values.tolist(),
+        "type": "bar"
+    }
+    return jsonify(trace)
+
+
+@app.route("/children")
+def children_data():
+    """Return state and museum type"""
+
+    # Query for the count of museums by type, by state
+    results = db.session.query(Museums.state, func.count(Museums.museumtype)).\
+    filter_by(museumtype = "CHILDREN'S MUSEUM").\
+    group_by(Museums.state).all()
+
+    df = pd.DataFrame(results, columns=['states', 'type_count'])
+
+    # Format the data for Plotly
+    trace = {
+        "x": df["states"].values.tolist(),
+        "y": df["type_count"].values.tolist(),
+        "type": "bar"
+    }
+    return jsonify(trace)
+
+@app.route("/general")
+def general_data():
+    """Return state and museum type"""
+
+    # Query for the count of museums by type, by state
+    results = db.session.query(Museums.state, func.count(Museums.museumtype)).\
+    filter_by(museumtype = 'GENERAL MUSEUM').\
+    group_by(Museums.state).all()
+
+    df = pd.DataFrame(results, columns=['states', 'type_count'])
+
+    # Format the data for Plotly
+    trace = {
+        "x": df["states"].values.tolist(),
+        "y": df["type_count"].values.tolist(),
+        "type": "bar"
+    }
+    return jsonify(trace)
+
+@app.route("/historic")
+def historic_data():
+    """Return state and museum type"""
+
+    # Query for the count of museums by type, by state
+    results = db.session.query(Museums.state, func.count(Museums.museumtype)).\
+    filter_by(museumtype = 'HISTORIC PRESERVATION').\
+    group_by(Museums.state).all()
+
+    df = pd.DataFrame(results, columns=['states', 'type_count'])
+
+    # Format the data for Plotly
+    trace = {
+        "x": df["states"].values.tolist(),
+        "y": df["type_count"].values.tolist(),
+        "type": "bar"
+    }
+    return jsonify(trace)
+
+@app.route("/history")
+def history_data():
+    """Return state and museum type"""
+
+    # Query for the count of museums by type, by state
+    results = db.session.query(Museums.state, func.count(Museums.museumtype)).\
+    filter_by(museumtype = 'HISTORY MUSEUM').\
+    group_by(Museums.state).all()
+
+    df = pd.DataFrame(results, columns=['states', 'type_count'])
+
+    # Format the data for Plotly
+    trace = {
+        "x": df["states"].values.tolist(),
+        "y": df["type_count"].values.tolist(),
+        "type": "bar"
+    }
+    return jsonify(trace)
+
+@app.route("/natural")
+def natural_data():
+    """Return state and museum type"""
+
+    # Query for the count of museums by type, by state
+    results = db.session.query(Museums.state, func.count(Museums.museumtype)).\
+    filter_by(museumtype = 'NATURAL HISTORY MUSEUM').\
+    group_by(Museums.state).all()
+
+    df = pd.DataFrame(results, columns=['states', 'type_count'])
+
+    # Format the data for Plotly
+    trace = {
+        "x": df["states"].values.tolist(),
+        "y": df["type_count"].values.tolist(),
+        "type": "bar"
+    }
+    return jsonify(trace)
+
+@app.route("/science")
+def science_data():
+    """Return state and museum type"""
+
+    # Query for the count of museums by type, by state
+    results = db.session.query(Museums.state, func.count(Museums.museumtype)).\
+    filter_by(museumtype = 'SCIENCE & TECHNOLOGY MUSEUM OR PLANETARIUM').\
+    group_by(Museums.state).all()
+
+    df = pd.DataFrame(results, columns=['states', 'type_count'])
+
+    # Format the data for Plotly
+    trace = {
+        "x": df["states"].values.tolist(),
+        "y": df["type_count"].values.tolist(),
+        "type": "bar"
+    }
+    return jsonify(trace)
+
+@app.route("/zoo")
+def zoo_data():
+    """Return state and museum type"""
+
+    # Query for the count of museums by type, by state
+    results = db.session.query(Museums.state, func.count(Museums.museumtype)).\
+    filter_by(museumtype = 'ZOO, AQUARIUM, OR WILDLIFE CONSERVATION').\
+    group_by(Museums.state).all()
+
+    df = pd.DataFrame(results, columns=['states', 'type_count'])
+
+    # Format the data for Plotly
+    trace = {
+        "x": df["states"].values.tolist(),
+        "y": df["type_count"].values.tolist(),
+        "type": "bar"
+    }
+    return jsonify(trace)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ attempt3/static/js/app.js
@@ -0,0 +1,190 @@
+// from data.js
+var tableData = data;
+
+// get table references
+var tbody = d3.select("tbody");
+
+function buildTable(data) {
+  // First, clear out any existing data
+  tbody.html("");
+
+  // Next, loop through each object in the data
+  // and append a row and cells for each value in the row
+  data.forEach((dataRow) => {
+    // Append a row to the table body
+    var row = tbody.append("tr");
+
+    // Loop through each field in the dataRow and add
+    // each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+      var cell = row.append("td");
+        cell.text(val);
+      }
+    );
+  });
+}
+
+function handleClick() {
+
+  // Prevent the form from refreshing the page
+  d3.event.preventDefault();
+
+  // Grab the datetime value from the filter
+  // var st = d3.select("#State2").property("value");
+  var st = d3.select("#State").property("value");
+  let filteredData = tableData;
+
+  // Check to see if a date was entered and filter the
+  // data using that date.
+  if (st) {
+    // Apply `filter` to the table data to only keep the
+    // rows where the `datetime` value matches the filter value
+    filteredData = filteredData.filter(row => row.State === st);
+  }
+
+  // Rebuild the table using the filtered data
+  // @NOTE: If no date was entered, then filteredData will
+
+  // just be the original tableData.
+  buildTable(filteredData);
+}
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
+
+
+// Plot the default route once the page loads
+var defaultURL = "/botanical";
+d3.json(defaultURL).then(function(data) {
+  var data = [data];
+  var layout = { margin: { t: 30, b: 100 } };
+  Plotly.plot("bar", data, layout);
+});
+
+// Update the plot with new data
+function updatePlotly(newdata) {
+  Plotly.restyle("bar", "x", [newdata.x]);
+  Plotly.restyle("bar", "y", [newdata.y]);
+}
+
+// Get new data whenever the dropdown selection changes
+function getData(route) {
+  console.log(route);
+  d3.json(`/${route}`).then(function(data) {
+    console.log("newdata", data);
+    updatePlotly(data);
+  });
+}
+
+
+
+FusionCharts.ready(function() {
+  var CSIGauge = new FusionCharts({
+      type: 'hlineargauge',
+      renderAt: 'chart-container',
+      id: 'cpu-linear-gauge-4',
+      width: '400',
+      height: '170',
+      manageresize: "1",
+      showValue: "1",
+      dataFormat: 'json',
+      dataSource: {
+        "chart": {
+          "theme": "fusion",
+          "caption": "Rate your favorite museum",
+          "lowerLimit": "0",
+          "upperLimit": "10",
+          // "numberSuffix": "%",
+          "chartBottomMargin": "20",
+          "valueFontSize": "11",
+          "valueFontBold": "0",
+          "gaugeFillMix": "{light-10},{light-70},{dark-10}",
+          "gaugeFillRatio": "40,20,40",
+          "editMode": "1"
+        },
+        "colorRange": {
+          "color": [{
+            "minValue": "0",
+            "maxValue": "4",
+            "code": "#F2726F"
+
+          }, {
+            "minValue": "4",
+            "maxValue": "7",
+            "code": "#FFC533"
+
+          }, {
+            "minValue": "7",
+            "maxValue": "10",
+            "code": "#62B58F"
+
+          }]
+        },
+        "pointers": {
+          "pointer": [{
+            "value": "0"
+          }]
+        }
+      },
+      "events": {
+        'beforeRender': function(evt, args) {
+          // creating div for controllers
+          var controllers = document.createElement('div');
+
+          // Create radio buttons inside div
+          controllers.innerHTML = '<input type="button" value="Get Data" id="getdata_btn" style="margin-left:5px;margin-top:5px;"/><div id="tableView" style="margin: 3px;padding:5px;float: left;"></div>';
+          // set css style for controllers div
+          controllers.style.cssText = '';
+          args.container.appendChild(controllers);
+          controllers.setAttribute('id', 'controllers');
+        },
+        "renderComplete": function(evtObj, argObj) {
+          evtObj.sender.intervalVar = setInterval(function() {
+            // var prcnt = 65 + parseInt(Math.floor(Math.random() * 10), 10);
+            // var prcnt = parseInt(Math.floor, 10);
+            FusionCharts.items["cpu-linear-gauge-4"].feedData("value=");
+          }, 5000);
+
+          //Format minutes, seconds by adding 0 prefix accordingly
+          function formatTime(time) {
+            (time < 10) ? (time = "0" + time) : (time = time);
+            return time;
+          }
+
+          function showData() {
+            //Retrieving the data
+            var dataVal = FusionCharts.items["cpu-linear-gauge-4"].getData(1),
+              str = "",
+              currDate = new Date(),
+              label = formatTime(currDate.getHours()) + ":" + formatTime(currDate.getMinutes()) + ":" + formatTime(currDate.getSeconds());
+            //Creating the table format
+            str += '<table border="1" cellpadding="1" cellspacing="0" bordercolor="#cccccc" width="320px">';
+            str += '    <tr>';
+            str += '        <td width="50%" style="font-weight: bold;font-size: 14px;padding: 3px;">Current Time</td>';
+            str += '        <td width="50%" style="font-weight: bold;font-size: 14px;padding: 3px;">Current Rating</td>';
+            str += '    </tr>';
+            str += '    <tr>';
+            str += '        <td width="50%" style="padding: 3px;" align="center">' + label + '</td>';
+            str += '        <td width="50%" style="padding: 3px;" align="center">' + dataVal + '</td>';
+            str += '    </tr>';
+            //Preparing html string to create the table with data
+
+            str += '</table>';
+            //Showing the table
+            document.getElementById("tableView").style.cssText = 'overflow-y:auto;display:block;margin-top:5px';
+            document.getElementById("tableView").innerHTML = str;
+          }
+
+          document.getElementById("getdata_btn").addEventListener("click", showData);
+
+        },
+        "disposed": function(evtObj, argObj) {
+          clearInterval(evtObj.sender.intervalVar);
+        }
+      }
+    })
+    .render();
+});
